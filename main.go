82ced3e927f156b8d8bd27f1ae22ff4cd5c2881e
AswinPuthukaatil <aswin100396@gmail.com>
2025-04-26 13:56:14 +0530

    added a auto migration for the tables created so far

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"log"
+	"github.com/joho/godotenv"
+	"github.com/Prototype-1/freelanceX_project.crm_service/migrations" 
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: .env file not found, relying on system environment")
+	}
+}
+
+func main() {
+	db := migrations.ConnectDatabase()
+
+	migrations.RunMigrations(db)
+
+}
---
+++ migrations/migrate.go
@@ -0,0 +1,48 @@
+package migrations
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"github.com/Prototype-1/freelanceX_project.crm_service/internal/model" 
+)
+
+func ConnectDatabase() *gorm.DB {
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=UTC",
+		dbHost, dbPort, dbUser, dbPassword, dbName,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		NowFunc: func() time.Time {
+			return time.Now().UTC()
+		},
+	})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	return db
+}
+
+func RunMigrations(db *gorm.DB) {
+	err := db.AutoMigrate(
+		&models.Client{},
+		&models.Project{},
+	)
+	if err != nil {
+		log.Fatal("Failed to migrate database:", err)
+	}
+
+	log.Println("📦 Database migrated successfully!")
+}
